f3f5c6217d2b1cc0aa3710d70bd6d5074bd310b9
mrbqble <asetbek.b.1003@gmail.com>
2022-11-06 03:18:19 +0600

    2

--- src/actions/add.js
+++ src/actions/add.js
@@ -1,55 +1,55 @@
-import axios from "axios";
+import axios from 'axios';
 
-const link = "https://frozen-thicket-00434.herokuapp.com/api"
+const link = `https://frozen-thicket-00434.herokuapp.com/api/add`;
 
 export const getEvents = async () => {
-    const response = await axios.post('http://localhost:1500/api/add/events');
+    const response = await axios.post(`${link}/events`);
     return response.data;
 };
 
 export const getUsers = async () => {
-    const response = await axios.post('http://localhost:1500/api/add/users');
+    const response = await axios.post(`${link}/users`);
     return response.data;
 };
 
 export const certCode = async (code, email) => {
-    await axios.put('http://localhost:1500/api/add/code', {code, email});
+    await axios.put(`${link}/code`, {code, email});
 };
 
 export const status = async (users) => {
-    const response = await axios.put('http://localhost:1500/api/add/status', {users});
+    const response = await axios.put(`${link}/status`, {users});
     return response.data.message;
 };
 
 export const check = async (code) => {
-    const response = await axios.post('http://localhost:1500/api/add/check', {code});
+    const response = await axios.post(`${link}/check`, {code});
     return response.data;
 };
 
 export const attend = async (email, name, _id) => {
-    await axios.post('http://localhost:1500/api/add/attend', {email, name, _id});
+    await axios.post(`${link}/attend`, {email, name, _id});
 };
 
 export const leave = async (email, _id) => {
-    await axios.post('http://localhost:1500/api/add/leave', {email, _id});
+    await axios.post(`${link}/leave`, {email, _id});
 }
 
 export const report = async (bags, type, eventid, addinfo, attended, distance, coordinator) => {
-    const response = await axios.post('http://localhost:1500/api/add/report', {bags, type, eventid, addinfo, attended, distance, coordinator});
+    const response = await axios.post(`${link}/report`, {bags, type, eventid, addinfo, attended, distance, coordinator});
     alert(response.data.message)
 }
 
 export const getEducation = async () => {
-    const response = await axios.post("http://localhost:1500/api/add/education");
+    const response = await axios.post(`${link}/education`);
     return response.data;
 }
 
 export const addUniversity = async (university) => {
-    const response = await axios.post("http://localhost:1500/api/add/university", {name: university});
+    const response = await axios.post(`${link}/university`, {name: university});
     return response.data;
 }
 
 export const addEducation = async (name, city, school, college) => {
-    const response = await axios.post("http://localhost:1500/api/add/addEdu", {name, city, school, college});
+    const response = await axios.post(`${link}/addEdu`, {name, city, school, college});
     return response.data;
 }
--- src/actions/user.js
+++ src/actions/user.js
@@ -1,35 +1,37 @@
-import axios from "axios";
+import axios from 'axios';
+
+const link = `https://frozen-thicket-00434.herokuapp.com/api/auth`;
 
 export const profile = async (email, token) => {
-    const response = await axios.post('http://localhost:1500/api/auth/profile', {email, token});
-    localStorage.setItem('user', response.data);
+    const response = await axios.post(`${link}/profile`, {email, token});
+    localStorage.setItem(`user`, response.data);
     return response.data;
 };
 
 export const login = async (email, password, handleSetIsAuth) => {
-    const response = await axios.post('http://localhost:1500/api/auth/login', {email, password});
+    const response = await axios.post(`${link}/login`, {email, password});
     if (!response.data.message) {
         handleSetIsAuth();
-        localStorage.setItem('token', response.data);
+        localStorage.setItem(`token`, response.data);
     }
     return response;
 };
 
 export const registration = async (item) => {
     console.log(item);
-    await axios.post('http://localhost:1500/api/auth/registration', {item});
+    await axios.post(`${link}/registration`, {item});
 };
 
 export const editProfile = async (item) => {
-    await axios.post('http://localhost:1500/api/auth/edit', {item});
+    await axios.post(`${link}/edit`, {item});
 };
 
 export const profilePhoto = async (email, file) => {
-    const response = await axios.post("http://localhost:1500/api/auth/profilePhoto", {email, file});
+    const response = await axios.post(`${link}/profilePhoto`, {email, file});
     console.log(response.data);
 }
 
 export const getPlaces = async (latitude, longtitude) => {
-    const response = await axios.post("http://localhost:1500/api/auth/places", {latitude, longtitude});
+    const response = await axios.post(`${link}/places`, {latitude, longtitude});
     return response.data;
 }
